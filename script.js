86e0b3d15b478107a1a1d54fc1e11b2d4e74447a
Matthew Asmolo <matthewasmolo@gmail.com>
2026-07-28 17:20:11 +0800

    commit 3 index.html and script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const incrementbtn = document.querySelector('#increment');
+const decrementbtn = document.querySelector('#decrement');
+const resetbtn = document.querySelector('#reset');
 const display = document.getElementById('display');
 
 incrementbtn.addEventListener('click', () => {
   score++;
+  display.textContent = `Score: ${score}`;
+});
+
+decrementbtn.addEventListener('click', () => {
+  score--;
+  display.textContent = `Score: ${score}`;
+});
+
+resetbtn.addEventListener('click', () => {
+  score = 0;
   display.textContent = `Score: ${score}`;
 });
